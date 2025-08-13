3c565a1f1f8f2656b694ec98388cf7e876f61365
acalinux86 <aacali6777@gmail.com>
2025-08-13 21:41:33 +0300

    New Object Functions and Helper Functions

--- src/breakoutt.cpp
+++ src/breakoutt.cpp
@@ -11,6 +11,33 @@ void Ball::stats() const
     printf("    Radius: %.2f\n", Radius);
 }
 
+Vector2 Ball::GetPosition() const
+{
+    return Position;
+}
+
+float Ball::GetRadius() const
+{
+    return Radius;
+}
+
+void Ball::SetPosition(Vector2 NewPosition)
+{
+    Position = NewPosition;
+}
+
+void Ball::SetRadius(float Radius)
+{
+    Radius = Radius;
+}
+
+void Ball::BallMove(Vector2 moveValue)
+{
+    Vector2 Pos = GetPosition();
+    Pos = Pos + moveValue;
+    SetPosition(Pos);
+}
+
 Tile::Tile(Vector2 position, Vector2 size):
     Position(position), Size(size) {}
 
